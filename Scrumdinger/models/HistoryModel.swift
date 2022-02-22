81779ccae14fdbd32f30a6bfd6e6e7a2c9f169e7
Michael <ndiritumichael5@gmail.com>
2022-02-22 22:56:49 +0300

    Adding a new meeting Recording history of minutes

---
+++ Scrumdinger/models/HistoryModel.swift
@@ -0,0 +1,22 @@
+//
+//  HistoryModel.swift
+//  Scrumdinger
+//
+//  Created by mac on 22/02/2022.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5) {
+           self.id = id
+           self.date = date
+           self.attendees = attendees
+           self.lengthInMinutes = lengthInMinutes
+       }
+}
